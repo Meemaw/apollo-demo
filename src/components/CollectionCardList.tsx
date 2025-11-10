6e70b51f891d43a5460a0f9ed2724db101806bca
Meemaw <ematej.snuderl@gmail.com>
2025-11-10 13:22:10 +0100

    work

--- src/components/CollectionCardList.tsx
+++ src/components/CollectionCardList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { type FragmentType, gql, type TypedDocumentNode } from "@apollo/client";
-import { useFragment as readFragmentData } from "@apollo/client/react";
+import { useFragment as readFragmentData, useSuspenseFragment } from "@apollo/client/react";
 import { COLLECTION_CARD_FRAGMENT, CollectionCard } from "./CollectionCard";
 import type { CollectionCardListFragment } from "./CollectionCardList.generated";
 
@@ -19,15 +19,16 @@ type Props = {
 };
 
 export function CollectionCardList({ collections }: Props) {
+  const { data } = useSuspenseFragment({
+    from: collections,
+    fragmentName: "CollectionCardList",
+    fragment: COLLECTION_CARD_LIST_FRAGMENT,
+  });
+
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {collections.map((collection) => {
-        const { data } = readFragmentData({
-          from: collection,
-          fragmentName: "CollectionCardList",
-          fragment: COLLECTION_CARD_LIST_FRAGMENT,
-        });
-        return <CollectionCard collection={data} key={data.id} />;
+      {data.map((item) => {
+        return <CollectionCard collection={item} key={item.id} />;
       })}
     </div>
   );
